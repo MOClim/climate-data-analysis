b3ac0f0ce07d60e3dd33acb4377c93673856f4e5
MOClim <74119608+MOClim@users.noreply.github.com>
2026-05-04 12:54:47 -0600

    Add Day03 codes and sync README updates

---
+++ python/week02_basics_timeseries1/w02_03_multi-lines-loop.sample.py
@@ -0,0 +1,14 @@
+# Create a list contains the fruit name.
+fruits=["apple","cherry","peach","pear","banana","grape","watermelon","orange","kiwi","mango"]
+
+# simple printout
+print(fruits)
+
+# The function len() returns the total count of elements within the iterable
+print(len(fruits))
+print("")
+
+print("Index-based loop")
+# Uses an index to access each element of the list by its position
+for i in range(len(fruits)):
+  print(i,fruits[i])
