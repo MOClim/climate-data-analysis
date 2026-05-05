218f351dc974c0c11c6aeb76d5d18a5cafb09f39
Megumi Chikamoto <megchika@Megumis-MacBook-Air-2.local>
2026-05-05 01:05:58 -0600

    Start week03

---
+++ python/week03_timeseries2_pattern_analysis/w02_08_xy-plot.ocn.sample.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+import pandas as pd
+import matplotlib.pyplot as plt
+
+file_path = Path('../../data/NOAA.1850-2025.OCN.csv')
+
+data = pd.read_csv(file_path, comment='#')
+
+print(data.head())
+
+plt.plot(data['Year'], data['Anomaly'], marker='o')
+
+output_path = Path(__file__).with_suffix('.jpg')
+plt.savefig(output_path, dpi=300)
+
+plt.show()
+
