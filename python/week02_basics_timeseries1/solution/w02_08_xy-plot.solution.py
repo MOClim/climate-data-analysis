87c19975652994a243cbbda4cb3da796c4735b44
MOClim <74119608+MOClim@users.noreply.github.com>
2026-05-04 23:11:37 -0600

    Add w02_08 solutions

---
+++ python/week02_basics_timeseries1/solution/w02_08_xy-plot.solution.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+import pandas as pd
+import matplotlib.pyplot as plt
+
+file_path = Path('../../data/NOAA.1850-2025.OCN.csv')
+
+data = pd.read_csv(file_path, skiprows=4, names=['Year', 'Anomaly'])
+
+print(data.head())
+
+plt.plot(data['Year'], data['Anomaly'], marker='o')
+
+plt.xlabel('Year')
+plt.ylabel('Temperature Anomaly (°C)')
+plt.title('Global Ocean Temperature Anomalies')
+plt.grid(True)
+
+output_path = Path(__file__).with_suffix('.jpg')
+plt.savefig(output_path, dpi=300)
+
+plt.show()
+
