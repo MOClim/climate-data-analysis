dd683efa0775bcce4ff2adfe9d35691a75d18154
MOClim <74119608+MOClim@users.noreply.github.com>
2026-09-16 16:07:19 -0600

    add vi training

---
+++ python/vi_training/exercise1.sample.py
@@ -0,0 +1,31 @@
+# STEP 1: Type print("test")
+# Press i to enter INSERT mode.
+# After typing, press ESC to return to NORMAL mode.
+
+
+# STEP 2: Copy and paste
+# In NORMAL mode, move your cursor to the print("test") line.
+# Press Shift + Y to copy the line.
+# Move the cursor to the line below, and then press p.
+
+
+# STEP 3: Multiple copy and paste
+# Move your cursor to the print("test") line in STEP 2.
+# Press Shift + Y to copy the line.
+# Move the cursor to the line below,
+# and then press p 10 times.
+
+
+# STEP 4: Another way to copy and paste multiple lines
+# Move your cursor to the first print("test") line in STEP 3.
+# Type 10Y to copy 10 lines.
+# Move the cursor to the line below,
+# and then press p.
+
+
+# If you don't want to save your edits,
+# type :q! and press Enter.
+
+# If you want to save your edits,
+# press Shift + ZZ
+# or type :wq and press Enter.
